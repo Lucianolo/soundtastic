b2384153252d759521280f2d2df5dcadda11f866
lucianolo <luciano.ursache@gmail.com>
2018-06-17 18:41:24 +0200

    BE-2 adds song scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_17_161703) do
+ActiveRecord::Schema.define(version: 2018_06_17_163702) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.integer "length", default: 0
+    t.string "cover_url"
+    t.integer "rating", default: 0
+    t.integer "played_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
